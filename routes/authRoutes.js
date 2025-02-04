c050f618a5589dd5100dd193a116998611192604
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-04 22:14:49 +0545

    Routes(login, session, logout), Controller and Server.js configured and checked

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,16 @@ const router = express.Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.post("/logout", AuthController.logout);
 router.post('/forgot-password', AuthController.forgotPassword);
 router.post('/reset-password', AuthController.resetPassword);
+
+router.get("/session", (req, res) => {
+    if (req.session.user) {
+      res.json({ message: "User is logged in", user: req.session.user });
+    } else {
+      res.json({ message: "No active session" });
+    }
+  });
 
 module.exports = router;
